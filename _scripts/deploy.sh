e31db0ea59f1ac70d02c3bc5905b5e9f8a5f5af4
Travis Poulsen <travis.poulsen@gmail.com>
2016-06-06 09:20:14 -0400

    Add deploy step to config.

---
+++ _scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  # initalize new repo in _site and push to server
+  cd _site
+  git init
+
+  git remote add deploy "deploy@travispoulsen.com:/var/www/travispoulsen.com"
+  git config user.name "Travis CI"
+  git config user.email "travis.poulsen+travisci@gmail.com"
+
+  git add .
+  git commit -m "Deploy"
+  git push --force deploy master
+else
+  echo "Not master, not deploying."
+fi
