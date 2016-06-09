bdb5d43133cd263058ed610cc24c9917c8fc176f
Travis Poulsen <travis@smartlogic.io>
2016-06-09 09:35:55 -0400

    Cache ghc and cabal dirs to reduce build times
    This caches `$HOME/.ghc` and `$HOME/.cabal` to reduce build times from
    ~25 minutes to ~2 minutes (long build times caused by building all of
    the dependencies of the project each time).
    
    Additionally, I'm no longer building in a cabal sandbox. Sandboxing
    isn't needed on the CI instance.

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,5 @@
 
 set -x
 
-openssl aes-256-cbc -K $encrypted_2fcc66f61dc1_key -iv $encrypted_2fcc66f61dc1_iv -in deploy-key.enc -out deploy-key -d
-rm deploy-key.enc
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
+cabal update
+cabal install --only-dependencies
