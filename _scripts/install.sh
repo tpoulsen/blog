069a65836643a07211864e4d224bb753cdf6656b
Travis Poulsen <travis.poulsen@gmail.com>
2017-03-03 09:51:04 -0500

    Switch to using stack for building

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-set -x
-
-cabal update
-cabal install --only-dependencies
+- echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
+- if [ -f configure.ac ]; then autoreconf -i; fi
+- |
+    set -ex
+case "$BUILD" in
+    stack)
+        stack --no-terminal --install-ghc $ARGS test --bench --only-dependencies
+        ;;
+    cabal)
+        cabal --version
+        travis_retry cabal update
+        cabal install --only-dependencies --enable-tests --enable-benchmarks --force-reinstalls --ghc-options=-O0 --reorder-goals --max-backjumps=-1 $CABALARGS $PACKAGES
+        ;;
+esac
+set +ex
--- _scripts/pre_install.sh
+++ _scripts/pre_install.sh
@@ -6,3 +6,27 @@ openssl aes-256-cbc -K $encrypted_2fcc66f61dc1_key -iv $encrypted_2fcc66f61dc1_i
 rm deploy-key.enc
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
+
+# Download and unpack the stack executable
+export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$HOME/.local/bin:/opt/alex/$ALEXVER/bin:/opt/happy/$HAPPYVER/bin:$HOME/.cabal/bin:$PATH
+mkdir -p ~/.local/bin
+|
+if [ `uname` = "Darwin" ]
+then
+  travis_retry curl --insecure -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
+else
+  travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+fi
+
+# Use the more reliable S3 mirror of Hackage
+mkdir -p $HOME/.cabal
+echo 'remote-repo: hackage.haskell.org:http://hackage.fpcomplete.com/' > $HOME/.cabal/config
+echo 'remote-repo-cache: $HOME/.cabal/packages' >> $HOME/.cabal/config
+
+if [ "$CABALVER" != "1.16" ]
+then
+  echo 'jobs: $ncpus' >> $HOME/.cabal/config
+fi
+
+# Get the list of packages from the stack.yaml file
+PACKAGES=$(stack --install-ghc query locals | grep '^ *path' | sed 's@^ *path:@@')
